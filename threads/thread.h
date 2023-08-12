64965f733ab8588e83f5fab16e05b4abdd8ee778
J2-HUSSAINRASH <Juwairiya2.Hussainrasheed@live.uwe.ac.uk>
2023-08-12 19:04:02 +0500

    memory issue and arguments from program arguments solved. exit code has been added.

--- threads/thread.h
+++ threads/thread.h
@@ -96,6 +96,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    int8_t* exitcode;                       /*exit code */
 #endif
 
     /* Owned by thread.c. */
--- userprog/process.c
+++ userprog/process.c
@@ -21,6 +21,12 @@
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
+/*to get the program name from an argument using strok_r*/
+char* get_program (const char* file_name){
+char* _ptr = NULL;
+char* _val = strtok_r(file_name, " ", &_ptr);
+return _val;
+}
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
@@ -37,9 +43,12 @@ process_execute (const char *file_name)
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
+  
+ /*getting the program name from argumentt*/
+  char* _name = get_program(file_name);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  thread_create (_name, PRI_DEFAULT, start_process, fn_copy); 
 
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
@@ -61,7 +70,10 @@ start_process (void *file_name_)
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
 
-  success = load (file_name, &if_.eip, &if_.esp);
+/*getting program name from argument*/
+
+char* _name = get_program(file_name);
+success = load (_name, &if_.eip, &if_.esp);
   
   /* If load failed, quit. */
   palloc_free_page (file_name);
@@ -119,6 +131,9 @@ process_exit (void)
       pagedir_activate (NULL);
       pagedir_destroy (pd);
     }
+/*printing the exit code*/
+printf("%s: exit code: %d\n", cur -> name, cur -> exitcode);
+
 }
 
 /* Sets up the CPU for running user code in the current
@@ -443,7 +458,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success) {
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;
       } else
         palloc_free_page (kpage);
     }
